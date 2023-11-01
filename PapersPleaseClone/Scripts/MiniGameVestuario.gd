212672e9a7dd2c0c895edc4d6a18a2ab995613b4
João Anisio <joaoanisiomn@hotmail.com>
2023-11-01 09:43:27 -0300

    Jogo do Vestuario estados iniciais implementados

--- PapersPleaseClone/Scripts/MiniGameVestuario.gd
+++ PapersPleaseClone/Scripts/MiniGameVestuario.gd
@@ -1,10 +1,12 @@
 extends Control
 
 @onready var rubrica = $Rubrica/ColorRect
-
+@export var dialogos : Resource
 var aleatorizar = RandomNumberGenerator
 
 var dentroRubrica = false
+
+var resposta = 0
 
 var adequado = false
 
@@ -17,17 +19,23 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 	
 
 func proximoEnfermeiro():
+	$AnimationPlayer.play("NurseLeaving")
+	await($AnimationPlayer.animation_finished)
+	resposta = aleatorizar.randi_range(0, dialogos.enfermeiroDialogosEntrada.size()-1)
+	set_text(dialogos.enfermeiroDialogosEntrada[resposta])
 	adequado = aleatorizar.randi_range(0,1)
 	print(adequado)
 	if(adequado):
 		$Enfermeiro/Sprite2D.frame = 0
 	else:
 		$Enfermeiro/Sprite2D.frame = aleatorizar.randi_range(1,5)
+		print($Enfermeiro/Sprite2D.frame)
+	
+	$AnimationPlayer.play("NurseComing")
+	await($AnimationPlayer.animation_finished)
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and dentroRubrica:
@@ -35,15 +43,19 @@ func _input(event):
 			if(adequado):
 				proximoEnfermeiro()
 				$Feedback/Label.text = "Acertou"
+				set_text(dialogos.enfermeiroDialogoSaidaCorreto[resposta])
 			else:
 				proximoEnfermeiro()
+				set_text(dialogos.enfermeiroDialogoSaidaCorreto[resposta])
 				$Feedback/Label.text = "Errou, Kit Inompleto"
 		elif(corDoCarimbo == "vermelho"):
 			if(adequado == 0):
 				proximoEnfermeiro()
+				set_text(dialogos.enfermeiroDialogoSaidaErrado[resposta])
 				$Feedback/Label.text = "Acertou"
 			else:
 				proximoEnfermeiro()
+				set_text(dialogos.enfermeiroDialogoSaidaErrado[resposta])
 				$Feedback/Label.text = "Errou, Kit Completo"
 			
 func _on_area_2d_area_entered(area):
@@ -52,6 +64,8 @@ func _on_area_2d_area_entered(area):
 		corDoCarimbo = area.get_parent().tipo
 
 
+func set_text(text):
+	$Dialogo/Panel/Label.text = text 
 
 func _on_area_2d_area_exited(area):
 	if(area.is_in_group("Carimbo")):
---
+++ PapersPleaseClone/Scripts/miniDialogos.gd
@@ -0,0 +1,5 @@
+extends Resource
+
+var enfermeiroDialogosEntrada = ["Pronto para salvar vidas!", "Cheguei o quanto antes!", "Estou com o Kit Completo!", "Vamos logo com isso!", "Bom dia!", "Hoje será um ótimo dia!"]
+var enfermeiroDialogoSaidaErrado = ["Talvez não tão pronto?", "Eita me esqueci...", "Talvez não tão completo assim...", "Sabia que estava faltando algo", "Vixe como fui bobo", "Obrigado por me lembrar!"]
+var enfermeiroDialogoSaidaCorreto = ["E lá vamos nós!", "Não temos tempo a perder!", "Eu sabia que dessa vez eu estava!", "Te aguardo na sala", "Vai dar tudo certo!", "Vai ficar tudo bem!"]
