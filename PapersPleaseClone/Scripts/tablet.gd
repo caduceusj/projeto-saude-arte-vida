e9a99b3e1bd60dcf1bed3284f946371851c03aa3
davidonigit <davi.duarte11@gmail.com>
2023-11-06 19:55:03 -0300

    inicio prototipo livro -> tablet

---
+++ PapersPleaseClone/Scripts/tablet.gd
@@ -0,0 +1,13 @@
+extends Node2D
+var pag = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/CheckBox.set_pressed_no_signal(true)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
