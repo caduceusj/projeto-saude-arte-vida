bc5736b2b42919e52e93c9f75b367bbcb1b25b5f
Davi Duarte <davi.duarte11@gmail.com>
2023-11-06 21:21:38 -0300

    tablet com ScrollContainer

--- PapersPleaseClone/Scripts/tablet.gd
+++ PapersPleaseClone/Scripts/tablet.gd
@@ -1,13 +1,26 @@
 extends Node2D
-var pag = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/CheckBox.set_pressed_no_signal(true)
+	pass
 	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func _resetCheckBox():
+	$ScrollContainer/VBoxContainer/CheckBox.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox2.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox3.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox4.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox5.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox6.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox7.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox8.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox9.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox10.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox11.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox12.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox13.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox14.set_pressed_no_signal(false)
+	$ScrollContainer/VBoxContainer/CheckBox15.set_pressed_no_signal(false)
