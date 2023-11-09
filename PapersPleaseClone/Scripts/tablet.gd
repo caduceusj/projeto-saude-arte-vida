5927c1f39c0254e72b86068d269496bf6c9d27dd
davidonigit <davi.duarte11@gmail.com>
2023-11-09 18:40:26 -0300

    pagina conteudo do tablet

--- PapersPleaseClone/Scripts/tablet.gd
+++ PapersPleaseClone/Scripts/tablet.gd
@@ -2,13 +2,28 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$ScrollContainerConteudo.scale = Vector2(0,0)
+	$TextureButtonEsquerda.scale = Vector2(0,0)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _resetCheckBox():
-	for x in $ScrollContainer/VBoxContainer.get_child_count():
-		get_node("ScrollContainer/VBoxContainer/CheckBox"+str(x+1)).set_pressed_no_signal(false)
+	for x in $ScrollContainerCheckBox/VBoxContainer.get_child_count():
+		get_node("ScrollContainerCheckBox/VBoxContainer/CheckBox"+str(x+1)).set_pressed_no_signal(false)
 
+
+
+func _on_texture_button_esquerda_pressed():
+	$TextureButtonEsquerda.scale = Vector2(0,0)
+	$TextureButtonDireita.scale = Vector2(1,1)
+	$ScrollContainerConteudo.scale = Vector2(0,0)
+	$ScrollContainerCheckBox.scale = Vector2(1,1)
+
+
+func _on_texture_button_direita_pressed():
+	$TextureButtonDireita.scale = Vector2(0,0)
+	$TextureButtonEsquerda.scale = Vector2(1,1)
+	$ScrollContainerCheckBox.scale = Vector2(0,0)
+	$ScrollContainerConteudo.scale = Vector2(1,1)
